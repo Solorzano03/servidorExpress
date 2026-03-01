60cb96b38211a6370b37668bcecbabb6953a74eb
Diego Cedeño Conforme <90148838+Diego1202@users.noreply.github.com>
2026-03-01 20:12:07 +0000

    creacion de la base de datos nueva

--- utils/datasource.js
+++ utils/datasource.js
@@ -6,13 +6,13 @@ const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
   ssl: true,
-  logging: false,
-  synchronize: false,
+  logging: true,
+  synchronize: true,
   entities: [
     require('../Entity/users'),
     require('../Entity/juegos'),
     require('../Entity/tarjetas'),
     ],
-  //dropSchema: true, para eliminar esquema total cuando realice cambios en la api 
+  dropSchema: true, //para eliminar esquema total cuando realice cambios en la api 
 });
 module.exports = { AppDataSource };
